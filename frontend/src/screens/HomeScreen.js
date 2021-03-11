315d5cce914abde2e1a4f96787dde98a368f532a
Michael <michael.barry.email@gmail.com>
2021-03-10 22:28:18 -0700

    linked products to useEffect

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,21 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
 import "./HomeScreen.css";
 import Product from "../components/Product";
+import { getProducts as listProducts } from "../redux/actions/productActions";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
+  const getProducts = useSelector((state) => state.getProducts);
+  const { products, loading, error } = getProducts;
+
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
+
+  //need to add proxy to package.json "proxy": "http://127.0.0.1:3001",
   return (
     <div className="homescreen">
       <h2 className="homescreen__title">Latest Products</h2>
