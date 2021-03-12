33a402eccf186a435c11a7b5583543bfc04f702d
Michael <michael.barry.email@gmail.com>
2021-03-12 00:09:26 -0700

    heroku

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -11,11 +11,11 @@ const {
 //GET all of our products from DB
 //@route GET /api/products
 //@access public
-router.get("/taylor-shopping-cart/", getAllProducts);
+router.get("/", getAllProducts);
 
 //GET a product from DB
 //@route GET /api/products/:id
 //@access public
-router.get("/taylor-shopping-cart/:id", getProductById);
+router.get("/:id", getProductById);
 
 module.exports = router;
